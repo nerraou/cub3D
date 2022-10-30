750e744f6dff606f696e06cd0c1920e79cb8ad21
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2022-10-30 11:31:23 +0100

    complete minimap  raycasting

--- src/mlx/draw_rays.c
+++ src/mlx/draw_rays.c
@@ -6,14 +6,21 @@
 /*   By: nerraou <nerraou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 13:47:05 by nerraou           #+#    #+#             */
-/*   Updated: 2022/10/29 18:30:58 by nerraou          ###   ########.fr       */
+/*   Updated: 2022/10/30 11:25:53 by nerraou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mlx.h"
+#include "limits.h"
+
+float distance(float start_x, float start_y, float end_x, float end_y)
+{
+	return sqrtf(powf(end_x - start_x, 2.0f) + powf(end_y - start_y, 2.0f));
+}
 
 void cast_ray(t_data *data, t_ray *ray, t_map *map, float angle)
 {
+	(void)ray;
 	int is_ray_facing_down;
 	int is_ray_facing_right;
 	float y_intercept;
@@ -25,6 +32,17 @@ void cast_ray(t_data *data, t_ray *ray, t_map *map, float angle)
 
 	float verticalowallhit_x;
 	float verticalwallhit_y;
+	int find_horizontal;
+	int find_vertical;
+
+	find_horizontal = 0;
+	find_vertical = 0;
+
+	float horizont_dist;
+	float vertical_dist;
+
+	horizont_dist = INT_MAX;
+	vertical_dist = INT_MAX;
 
 	is_ray_facing_down = angle > 0.0f && angle < (float)M_PI;
 	is_ray_facing_right = angle > 3.0f * (float)M_PI_2 || angle < (float)M_PI_2;
@@ -53,6 +71,7 @@ void cast_ray(t_data *data, t_ray *ray, t_map *map, float angle)
 			fill(data, 0xF3DE2C);
 			horizontalwallhit_x = x_intercept;
 			horizontalwallhit_y = y_intercept;
+			find_horizontal = 1;
 			// draw_line(data, map->player.x, map->player.y, x_intercept, y_intercept);
 			break;
 		}
@@ -62,22 +81,23 @@ void cast_ray(t_data *data, t_ray *ray, t_map *map, float angle)
 			y_intercept += y_step;
 		}
 	}
-	y_intercept = floor(map->player.y / data->scale) * data->scale;
-	if (is_ray_facing_down)
-		y_intercept += data->scale;
-	x_intercept = map->player.x + (y_intercept - map->player.y) / tan(angle);
-	y_step = data->scale;
-	if (!is_ray_facing_down)
+	x_intercept = floor(map->player.x / data->scale) * data->scale;
+	if (is_ray_facing_right)
+		x_intercept += data->scale;
+	y_intercept = map->player.y + (x_intercept - map->player.x) * tan(angle);
+
+	x_step = data->scale;
+	if (!is_ray_facing_right)
+		x_step *= -1;
+
+	y_step = x_step * tan(angle);
+	if (!is_ray_facing_down && y_step > 0)
+		y_step *= -1;
+	else if (is_ray_facing_down && y_step < 0)
 		y_step *= -1;
 
-	x_step = y_step / tan(angle);
-	if (!is_ray_facing_right && x_step > 0)
-		x_step *= -1;
-	else if (is_ray_facing_right && x_step < 0)
-		x_step *= -1;
-
-	if (!is_ray_facing_down)
-		y_intercept--;
+	if (!is_ray_facing_right)
+		x_intercept--;
 	while (x_intercept >= 0 && x_intercept <= 34 * 20 && y_intercept >= 0 && y_intercept <= 14 * 20)
 
 	{
@@ -86,7 +106,7 @@ void cast_ray(t_data *data, t_ray *ray, t_map *map, float angle)
 			fill(data, 0xF3DE2C);
 			verticalowallhit_x = x_intercept;
 			verticalwallhit_y = y_intercept;
-			// draw_line(data, map->player.x, map->player.y, x_intercept, y_intercept);
+			find_vertical = 1;
 			break;
 		}
 		else
@@ -95,6 +115,18 @@ void cast_ray(t_data *data, t_ray *ray, t_map *map, float angle)
 			y_intercept += y_step;
 		}
 	}
+	if (find_horizontal == 1)
+	{
+		horizont_dist = distance(map->player.x, map->player.y, horizontalwallhit_x, horizontalwallhit_y);
+	}
+	if (find_vertical == 1)
+	{
+		vertical_dist = distance(map->player.x, map->player.y, verticalowallhit_x, verticalwallhit_y);
+	}
+	if (horizont_dist < vertical_dist)
+		draw_line(data, map->player.x, map->player.y, horizontalwallhit_x, horizontalwallhit_y);
+	else
+		draw_line(data, map->player.x, map->player.y, verticalowallhit_x, verticalwallhit_y);
 }
 
 float normalize_angle(float angle)
@@ -116,7 +148,7 @@ void draw_rays(t_data *data, t_ray *ray, t_map *map)
 	{
 		move_angle = normalize_angle(move_angle);
 		cast_ray(data, ray, map, move_angle);
-		// move_angle += ray->fov_angle / (34 * 20);
+		move_angle += ray->fov_angle / (34 * 20);
 		i++;
 	}
 }
