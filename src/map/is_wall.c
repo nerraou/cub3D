216c3da1029be6cfdf6a9c8ecfcb9d8510a67392
Youssef Bahlaoui <ybahlaoui@gear9.ma>
2022-11-22 23:22:56 +0100

    check boundaries in is_wall

--- src/map/is_wall.c
+++ src/map/is_wall.c
@@ -3,25 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   is_wall.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nerraou <nerraou@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ybahlaou <ybahlaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 16:06:06 by nerraou           #+#    #+#             */
-/*   Updated: 2022/11/22 10:34:49 by nerraou          ###   ########.fr       */
+/*   Updated: 2022/11/22 23:06:21 by ybahlaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 
-int	is_wall(int x, int y, int scale, char **map)
+int	is_wall(int x, int y, int scale, t_map *map)
 {
 	int	map_x;
 	int	map_y;
 
 	map_x = (x / scale);
 	map_y = (y / scale);
-	if (map[map_y] != NULL)
+	if (map_y < 0 || map_y >= map->height || map_x < 0 || map_x >= map->widths[map_y])
+		return (1);
+	if (map->map_array[map_y] != NULL)
 	{
-		if (map[map_y][map_x] == '1')
+		if (map->map_array[map_y][map_x] == '1')
 			return (1);
 	}
 	return (0);
