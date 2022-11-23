1f0abcddccbc52c955754f913a9c07b761e5a1cf
Youssef Bahlaoui <ybahlaoui@gear9.ma>
2022-11-24 00:01:26 +0100

    set player spawn postion to the center

--- src/map/set_replace_player_position.c
+++ src/map/set_replace_player_position.c
@@ -6,7 +6,7 @@
 /*   By: ybahlaou <ybahlaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 11:53:32 by nerraou           #+#    #+#             */
-/*   Updated: 2022/11/23 19:31:08 by ybahlaou         ###   ########.fr       */
+/*   Updated: 2022/11/23 23:57:45 by ybahlaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int	set_replace_player_position(t_map *data, int scale)
 			if (ft_indexof("NEWS", data->map_array[y][x]) != -1)
 			{
 				data->player_orientation = data->map_array[y][x];
-				data->player.x = x * scale;
-				data->player.y = y * scale;
+				data->player.x = x * scale + scale / 2;
+				data->player.y = y * scale + scale / 2;
 				data->map_array[y][x] = '0';
 				count++;
 			}
