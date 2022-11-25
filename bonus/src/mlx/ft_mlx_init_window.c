081f8d6fe61e931431fae6d90c615c91b36b35e9
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2022-11-25 22:30:42 +0100

    check for mlx failure

--- bonus/src/mlx/ft_mlx_init_window.c
+++ bonus/src/mlx/ft_mlx_init_window.c
@@ -6,7 +6,7 @@
 /*   By: nerraou <nerraou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 14:49:30 by nerraou           #+#    #+#             */
-/*   Updated: 2022/11/24 13:49:39 by nerraou          ###   ########.fr       */
+/*   Updated: 2022/11/25 22:29:06 by nerraou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ void	ft_mlx_init_window(t_data *data, const t_map *map)
 	data->mlx_win = mlx_new_window(data->mlx, data->width, data->height, \
 	"Cub3D");
 	data->img = mlx_new_image(data->mlx, data->width, data->height);
+	if (!data->img)
+		exit(1);
 	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, \
 	&data->line_length, &data->endian);
+	if (!data->addr)
+		exit(1);
 }
