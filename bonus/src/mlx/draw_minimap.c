fa30707753591fb7088eb1c2b629ece003284a8a
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2022-11-25 17:04:40 +0100

    fix door assets path and drawning minimap

--- bonus/src/mlx/draw_minimap.c
+++ bonus/src/mlx/draw_minimap.c
@@ -6,13 +6,13 @@
 /*   By: ybahlaou <ybahlaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 14:36:43 by nerraou           #+#    #+#             */
-/*   Updated: 2022/11/25 16:12:59 by ybahlaou         ###   ########.fr       */
+/*   Updated: 2022/11/25 16:33:04 by ybahlaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mlx.h"
 
- int	get_color(char cell_value)
+static int	get_color(char cell_value)
 {
 	if (cell_value == '1')
 		return (0xFFFFFF);
@@ -21,7 +21,7 @@
 	return (0x555555);
 }
 
- void	draw_arc(t_data *data, t_ray *ray, t_map *map)
+static void	draw_arc(t_data *data, t_ray *ray, t_map *map)
 {
 	float		move_angle;
 	int			i;
@@ -29,6 +29,7 @@
 	t_vector2	end;
 	t_player	*player;
 
+	fill(data, 0xff634F);
 	player = &map->minimap.player;
 	vector2_set(&start, player->x, player->y);
 	move_angle = map->player.rotation_angle - (ray->fov_angle / 2);
@@ -44,39 +45,47 @@
 	}
 }
 
+static int	is_out_of_boundaries(int x, int y, t_map *map)
+{
+	if (x < 0 || y < 0 || y >= map->height || x >= map->widths[y])
+		return (1);
+	return (0);
+}
+
+static void	set_player_coords(t_player *player, t_map *map,
+	int start_x, int start_y)
+{
+	t_minimap	*mmap;
+
+	mmap = &map->minimap;
+	player->x = (map->player.x / map->scale - start_x) * mmap->scale;
+	player->y = (map->player.y / map->scale - start_y) * mmap->scale;
+}
+
 void	draw_minimap(t_data *data, t_map *map, t_minimap *mmap)
 {
-	int			start_x;
-	int			start_y;
+	const int	s_x = map->player.x / map->scale - 10;
+	const int	s_y = map->player.y / map->scale - 10;
 	int			dx;
 	int			dy;
 	const int	size = 20;
 
-	start_x = map->player.x / map->scale - 10;
-	start_y = map->player.y / map->scale - 10;
-	mmap->player.x = (map->player.x / map->scale - start_x) * mmap->scale;
-	mmap->player.y = (map->player.y / map->scale - start_y) * mmap->scale;
+	set_player_coords(&mmap->player, map, s_x, s_y);
 	dy = 0;
 	while (dy < size)
 	{
 		dx = 0;
 		while (dx < size)
 		{
-			if (dx + start_x < 0 || dy + start_y < 0 || dy + start_y >= map->height || dx + start_x >= map->widths[dy + start_y])
-			{
-				fill(data, 0x0);
-				draw_rect(data, vector2_create(dx * mmap->scale, dy * mmap->scale), mmap->scale, mmap->scale);
-			}
-			else
-			{
-				fill(data, get_color(map->map_array[dy + start_y][dx + start_x]));
-				draw_rect(data, vector2_create(dx * mmap->scale, dy * mmap->scale), mmap->scale, mmap->scale);
-			}
+			fill(data, 0x0);
+			if (!is_out_of_boundaries(dx + s_x, dy + s_y, map))
+				fill(data, get_color(map->map_array[dy + s_y][dx + s_x]));
+			draw_rect(data, vector2_create(dx * mmap->scale, dy * mmap->scale),
+				mmap->scale, mmap->scale);
 			dx++;
 		}
 		dy++;
 	}
-	fill(data, 0xff634F);
 	draw_arc(data, map->ray, map);
 	draw_player(data, &mmap->player);
 }
